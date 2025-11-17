089ed20639150b0eb5543345cc4791e3396070d2
Abner Santos Alves <164810770+AbnerSantosAlves@users.noreply.github.com>
2025-11-16 22:52:03 -0300

    Update database.py

--- src/infra/config/database.py
+++ src/infra/config/database.py
@@ -1,25 +1,21 @@
-from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-# Criar engine usando apenas o arquivo local "meubd.db"
 engine = create_engine(
     "sqlite:///meubd.db",
     connect_args={"check_same_thread": False}
 )
 
-# Criar sessão
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-# Declarative base
 Base = declarative_base()
 
-# Criar tabelas
 def criar_db():
     Base.metadata.create_all(bind=engine)
 
-# Dependência (FastAPI ou uso manual)
 def get_db():
     db = SessionLocal()
     try:
         yield db
     finally:
+        db.close()
