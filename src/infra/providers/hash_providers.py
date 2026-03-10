6adb639da27b8a4b86c26bcfc85e9dfb23bb394a
rodion121 <164810770+AbnerSantosAlves@users.noreply.github.com>
2026-03-10 17:36:04 -0300

    Atualizando commit

--- src/infra/providers/hash_providers.py
+++
@@ -1,9 +0,0 @@
-from pwdlib import PasswordHash
-
-pwd_context = PasswordHash.recommended()
-
-def gerar_hash(texto: str) -> str:
-    return pwd_context.hash(texto)
-
-def verificar_hash(texto: str, hash: str) -> bool:
-    return pwd_context.verify(texto, hash)
